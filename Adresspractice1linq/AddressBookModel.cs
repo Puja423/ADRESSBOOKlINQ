391b7983f93ada5f03c9233fa7246258c508716e
Puja <pujayadav@gmail.com>
2020-11-12 15:41:22 +0530

    Add project files.

---
+++ Adresspractice1linq/AddressBookModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Data;
+
+namespace Adresspractice1linq
+{
+    class AddressBookModel
+    {
+        DataTable dataTable = new DataTable();
+    }
+}
---
+++ Adresspractice1linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Adresspractice1linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO ADDRESSBOOK");
+        }
+    }
+}
